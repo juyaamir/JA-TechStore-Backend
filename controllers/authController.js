d836c5c767fe5b743d08d1de365b7a3a4ba1c47e
Juya <juyaamirm@gmail.com>
2024-09-08 21:47:37 +0200

    Backend: bcryptjs is added and configured to hash the password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,26 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const registerUser = async (req, res) => {
+    
     try {
         const {name, email, password} = req.body;
         if(!name || !email || !password){
             return res.status(400).json({message: `Please provide all fields`});
         }
-        console.log(`Received data: ${JSON.stringify(req.body)}`)
+       /*  console.log(`Received data: ${JSON.stringify(req.body)}`) */
         //check if the user already exists
         const userExists = await User.findOne({email});
         if(userExists){
             return res.status(400).json({message: `User already exists`});
         }
         //create a new user
-        const user = new User({name, email, password});
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        
+        const user = new User({name, email, password: hashedPassword});
         await user.save();
-        res.status(201).json({message: `User created successfully`});
+        res.status(201).json({message: `Your account has been created successfully!`});
     } catch(error) {
         console.error(error);
         res.status(500).json({message: `Server Error`});
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { registerUser } from '../controllers/authController.js';
+
 const userRouter = express.Router();
 userRouter.post(`/register`, registerUser);
 export default userRouter;
